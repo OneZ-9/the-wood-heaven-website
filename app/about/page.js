159bf8a80be203b65d7392812aeef87a2d65aeac
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-08-09 15:17:02 +0530

    Initial commit for the-wood-heaven-website

---
+++ app/about/page.js
@@ -0,0 +1,5 @@
+function page() {
+  return <h1>About the Wood Heaven</h1>;
+}
+
+export default page;
---
+++ app/account/page.js
@@ -0,0 +1,5 @@
+function page() {
+  return <h1>Your account page</h1>;
+}
+
+export default page;
