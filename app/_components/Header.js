0ad3c6c5a5cf393849235539080b1d0e6e7536e3
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-08-21 04:39:14 +0530

    Changed Navigation component into NavMenu to resolve import

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,12 +1,12 @@
 import Logo from "@/app/_components/Logo";
-import Navigation from "./Navigation";
+import NavMenu from "@/app/_components/NavMenu";
 
 function Header() {
   return (
     <header className="border-b border-primary-900 px-8 py-5">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <Logo />
-        <Navigation />
+        <NavMenu />
       </div>
     </header>
   );
