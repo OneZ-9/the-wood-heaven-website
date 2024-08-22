8ac133e1feeab5983ad48a739613acb2c8f6756b
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-08-22 23:00:00 +0530

    Implemented create new record in DB for new sign-in users

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -11,6 +12,30 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user; // converted to boolean
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        // check if signed-in user already in DB
+        const existingGuest = await getGuest(user.email);
+
+        // if signed-in user is a new user then create a new user record
+        // important to await to get the details
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        // if no error
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+
+      //   get the guest details and then mutate the session object
+      session.user.guestId = guest.id;
+
+      return session;
     },
   },
   pages: {
